d87a3a9dae19cf97bf3baf801369d0b5ecb53e70
Ezequiel Barreto <ezequiel@Ezequiels-MacBook-Pro.local>
2016-10-09 16:57:54 -0500

    Cambios en la vista

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //handle the text field´s user input through delegate callbacks
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        //set the text from textfield to label whe click intro button in keyboard
+        if ((mealNameLabel.text?.isEmpty) == nil) {
+            mealNameLabel.text = textField.text
+            print("no esta vacio el textfield")
+        }
+        else{
+            mealNameLabel.text = "Debes ingresar un nombre"
+            print("esta vacio " + String(mealNameLabel.text?.isEmpty))
+        }
+        
+    }
+
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 
 }
 
